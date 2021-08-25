c2a153f451aa628ceebf815d9eaff8c1a73e8ad8
touhidpasha <touhidpasha552@gmail.com>
2021-08-25 11:40:14 +0530

    float max

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  class Largest{
-    public void findMax(int a,int b,int c){
+    public void intMax(int a,int b,int c){
       if(a>b){
       
 
@@ -15,6 +15,21 @@ import java.util.Scanner;
     else 
     System.out.println(c+ " is largest");
 }
+public void floatMax(Double a,Double b,Double c){
+    if(a>b){
+    
+
+      if(a>c)
+     System.out.println(a +"largest");
+     else
+     System.out.println(c+" is largest");
+    
+  }
+  else if(b>c)
+  System.out.println(b+" is largest");
+  else 
+  System.out.println(c+ " is largest");
+}
 
 }
 
@@ -22,6 +37,6 @@ public class Main{
     public static void main(String args[]){
         
         Largest l1=new Largest();
-        l1.findMax(1, 2, 3);
+        l1.floatMax(1.3, 3.6, 3.2);
  }
 }
