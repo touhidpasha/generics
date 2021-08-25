95234d096d130751e6b5f3855a02851a726d3153
touhidpasha <touhidpasha552@gmail.com>
2021-08-25 12:01:47 +0530

    largest for n num

--- Main.java
+++ Main.java
@@ -1,52 +1,23 @@
 import java.util.Scanner;
  class Largest{
-    public void intMax(int a,int b,int c){
-      if(a>b){
-      
-
-        if(a>c)
-       System.out.println(a +"largest");
-       else
-       System.out.println(c+" is largest");
-      
+    public void intMax(int... num){
+      int max=0;
+      for (int i: num)
+      {
+          if(i>max)
+          max=i;
+      }
+      System.out.println(max +"largest");
+}
+public void floatMax(float... num){
+    float max=0;
+    for (float i: num)
+    {
+        if(i>max)
+        max=i;
     }
-    else if(b>c)
-    System.out.println(b+" is largest");
-    else 
-    System.out.println(c+ " is largest");
+    System.out.println(max +"largest");
 }
-public void floatMax(Double a,Double b,Double c){
-    if(a>b){
-    
-
-      if(a>c)
-     System.out.println(a +"largest");
-     else
-     System.out.println(c+" is largest");
-    
-  }
-  else if(b>c)
-  System.out.println(b+" is largest");
-  else 
-  System.out.println(c+ " is largest");
-}
-
-public void stringMax(String a,String b,String c){
-    if(a.compareTo(b)>1){
-    
-
-      if(a.compareTo(c)>1)
-     System.out.println(a +" largest");
-     else
-     System.out.println(c+" is largest");
-    
-  }
-  else if(b.compareTo(c)>1)
-  System.out.println(b+" is largest");
-  else 
-  System.out.println(c+ " is largest");
-}
-
 
 }
 
@@ -54,7 +25,7 @@ public class Main{
     public static void main(String args[]){
         
         Largest l1=new Largest();
-        l1.stringMax("touhid","pasha","hello");
+        l1.intMax(1,2,3,20,7);
 
  }
 }
